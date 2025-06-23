333b55ac9400134cdb740f31801f1cae1233df26
codesterDiv <codesterDiv@gmail.com>
2025-06-23 19:30:19 +0530

    modified:   jarvis 4.0/head/brain.py

--- jarvis 4.0/head/brain.py
+++ jarvis 4.0/head/brain.py
@@ -1,11 +1,11 @@
 from google.cloud import discoveryengine_v1 as discoveryengine
 from google.api_core.client_options import ClientOptions
-
+import ear
 def answer_query(project_id: str, location: str, engine_id: str, query_text: str):
     client_options = ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
     client = discoveryengine.ConversationalSearchServiceClient(client_options=client_options)
 
-    serving_config = f"projects/{project_id}/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_serving_config"
+    serving_config = f"projects/jarvis-ai-463813/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_serving_config"
 
     request = discoveryengine.AnswerQueryRequest(
         serving_config=serving_config,
@@ -16,10 +16,10 @@ def answer_query(project_id: str, location: str, engine_id: str, query_text: str
     return response.answer_text
 
 # Example usage
-project_id = "your_project_id"
+project_id = "jarvis-ai-463813"
 location = "global"
 engine_id = "your_engine_id"
-query_text = "What is the capital of France?"
+query_text = ear.listen()
 
 answer = answer_query(project_id, location, engine_id, query_text)
 print(answer)
